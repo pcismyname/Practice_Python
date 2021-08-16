e91f21e44a736fed1f36b69dbc190c468fcf1748
pcismyname <pcismyname007@gmail.com>
2021-08-16 21:55:43 +0700

    4

---
+++ Divisors.py
@@ -0,0 +1,12 @@
+num = int(input("Enter number :"))
+print([i for i in range(1,num+1) if num%i == 0])
+
+
+"""listRange = list(range(1,num+1))
+divisorList = []
+
+for number in listRange:
+    if num % number == 0:
+        divisorList.append(number)"""
+
+
