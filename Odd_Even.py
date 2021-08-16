19e984d2d9dc1a434ad02ee13223d22318566bc8
pcismyname <pcismyname007@gmail.com>
2021-08-16 19:45:50 +0700

    2

---
+++ Odd_Even.py
@@ -0,0 +1,7 @@
+num = int(input("Enter number : "))
+if num%2 == 0:
+ print(str(num) + " is even number")
+ if num%4 ==0:
+     print(str(num) + " is also divided by 4")
+else :
+ print(str(num) + " is odd number")
