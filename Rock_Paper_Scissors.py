dfc37e80ec278c64d94b68c7289489fa158bfc9b
pcismyname <pcismyname007@gmail.com>
2021-08-19 23:00:36 +0700

    8_init

---
+++ Rock_Paper_Scissors.py
@@ -0,0 +1,25 @@
+print("Rock Paper Scissors Game")
+print("Press 1 to start")
+print("Press 0 to exit")
+n = int(input())
+if(n == 1):
+    while(1):
+        print("Rock == 1")
+        print("Paper == 2")
+        print("Scissors == 3")
+        p1 = int(input("player 1: "))
+        p2 = int(input("player 2: "))
+        
+
+
+
+
+        print("continue press 1 exit press 2")
+        c = int(input())
+        if(c==2):
+            break
+        
+        
+
+
+print("Exit Game!")
