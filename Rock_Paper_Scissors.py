683033f7f4d58fffd6a892abbc95234891647955
pcismyname <pcismyname007@gmail.com>
2021-08-20 22:55:40 +0700

    8

--- Rock_Paper_Scissors.py
+++ Rock_Paper_Scissors.py
@@ -9,17 +9,31 @@ if(n == 1):
         print("Scissors == 3")
         p1 = int(input("player 1: "))
         p2 = int(input("player 2: "))
-        
+        if(p1 == 1):
+            if(p2 == 1):
+                print("draw")
+            elif(p2 == 2):
+                print("player 2 win!")
+            else:
+                print("player 1 win!")
+        elif(p1 == 2):
+            if(p2 == 1):
+                print("player 2 win!")
+            elif(p2 == 2):
+                print("draw!")
+            else:
+                print("player 1 win!")
+        else:
+            if(p2 == 2):
+                print("player 1 win!")
+            elif(p2 == 2):
+                print("player 2 win!")
+            else:
+                print("draw!")
 
-
-
-
-        print("continue press 1 exit press 2")
+        print("continue press 1 ||| exit press 2")
         c = int(input())
         if(c==2):
             break
         
-        
-
-
 print("Exit Game!")
