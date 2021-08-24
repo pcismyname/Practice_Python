7acb05b83d0df76b428daf92ca294fca32b19a81
pcismyname <pcismyname007@gmail.com>
2021-08-24 09:52:55 +0700

    15

---
+++ Reverse_word.py
@@ -0,0 +1,11 @@
+def reverse_v2(x):
+  y = x.split()
+  return " ".join(y[::-1])
+
+sent = input("input your sentence :")
+split_sent = sent.split(" ")
+
+for i in range(len(split_sent)-1,-1,-1):
+    print(split_sent[i], end = " ")
+
+    
