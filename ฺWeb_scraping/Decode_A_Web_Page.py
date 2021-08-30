6cf5dd04d7fc052bc89f87b5eccd6259995fbf3b
pcismyname <pcismyname007@gmail.com>
2021-08-31 00:48:04 +0700

    scraperrrrrrrrrrr

--- ฺWeb_scraping/Decode_A_Web_Page.py
+++ ฺWeb_scraping/Decode_A_Web_Page.py
@@ -1,6 +1,7 @@
 import requests
-url = "https://www.nytimes.com/"
-r = requests.get(url)
-r_html = r.text
-print(r_html)
+from bs4 import BeautifulSoup
 
+html_text = requests.get("https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=")
+soup = BeautifulSoup(html_text.text, "lxml")
+jobs = soup.find_all("li", class_ = "clearfix job-bx wht-shd-bx")
+print(jobs)
