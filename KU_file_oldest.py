146ae40de2b5c5bbd0763fbb69b75061923b5fbf
pcismyname <pcismyname007@gmail.com>
2021-10-05 21:50:31 +0700

    KU

--- KU_file_oldest.py
+++ KU_file_oldest.py
@@ -12,7 +12,7 @@ for i in range(0,len(line)):
         name.append(data[j])
       elif(i==0 and j==1):
           max = int(data[j])
-          age.append(data[j])
+          age.append(int(data[j]))
       else:
           age.append(int(data[j]))
           if age[i]>max:
